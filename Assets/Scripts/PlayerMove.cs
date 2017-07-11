132732bfb6da261daa646476165bd1a197112e04
jorgellanos <jorg.llanos@hotmail.com>
2017-07-11 19:21:17 -0400

    julio salta
    cambie el script PlayerMove en ves  de if(puedesaltar = true) a
    if(puedesaltar)

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,10 +7,9 @@ public class PlayerMove : MonoBehaviour {
 
 
     public Vector2 speed = new Vector2(50,50);
-    private bool puedeSaltar = false;
+    public bool puedeSaltar = false;
     public int salto;
-
-    public LayerMask LMk;
+    
     public Rigidbody2D rb;
     public Animator ani;
     private Transform Pie;
@@ -24,9 +23,34 @@ public class PlayerMove : MonoBehaviour {
         rb = GetComponent<Rigidbody2D>();
         Pie = this.transform.Find("JulioPies");
     }
-	
-	// Update is called once per frame
-	void Update () {
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "floor")
+        {
+            puedeSaltar = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.tag == "floor")
+        {
+            puedeSaltar = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "floor")
+        {
+            puedeSaltar = false;
+        }
+    }
+    
+
+    // Update is called once per frame
+    void Update () {
         //-----Movement-----//
         float inputx = Input.GetAxis("Horizontal");
         Vector3 movement = new Vector3(speed.x * inputx, 0, 0);
@@ -35,28 +59,32 @@ public class PlayerMove : MonoBehaviour {
 
         if (Input.GetKeyDown("right"))
         {
-            ani.SetBool("Move", true);
+            
+
         }
         if (Input.GetKeyDown("left"))
         {
-            ani.SetBool("Move", true);
+            
         }
 
         if (Input.GetKeyUp("left") || Input.GetKeyUp("right"))
         {
-            ani.SetBool("Move", false);
+            
         }
 
 
         //-----Jump-----//
-        puedeSaltar = Physics2D.Linecast(this.transform.position, Pie.position, LMk);
         if (puedeSaltar)
         {
-            if (Input.GetKeyDown("space"))
+            if(Input.GetKeyDown("space"))
             {
                 rb.AddForce(Vector2.up * salto);
             }
         }
+            
+            
+            
+        
 
     }
 
