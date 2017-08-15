b42dbdc3c9055345b476eb1804f6bc8199002365
Ethese <F.goyeneche.camus@gmail.com>
2017-08-15 03:24:26 -0300

    added tree

--- Assets/Scripts/World/Menu.cs
+++ Assets/Scripts/World/Menu.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 namespace Cheche
 {
@@ -7,20 +8,45 @@ namespace Cheche
     {
         public GameObject UI;
         public bool activo;
+        bool Paused;
+        void Start()
+        {
+            Paused = false;
+        }
         void Update()
         {
-           // UI.SetActive(false);
+            Pause();
         }
 
         void OnTriggerEnter2D(Collider2D other)
         {
             UI.SetActive(true);
+            Text text = UI.GetComponent<Text>();
+            text.text = transform.name;
             activo = true;
         }
         void OnTriggerExit2D(Collider2D other)
         {
             UI.SetActive(false);
             activo = false;
+        }
+
+        void Pause()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Paused = !Paused;
+            }
+            if (Paused)
+            {
+                Time.timeScale = 0;
+            }
+            if (!Paused)
+            {
+                Time.timeScale = 1;
+            }
+
+
         }
 
     }
