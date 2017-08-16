d6d0f890d0873b1a3af0834921a8f939b1895b88
jorgellanos <jorg.llanos@hotmail.com>
2017-08-16 20:08:08 -0300

    julio puerta
    al presionar la telca e cerca del boton se abre la puerta(planeta)

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -1,61 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Viaje;
 
-public class Interact : MonoBehaviour {
 
-    //variable
-    private bool doar = false;
-
-    //reference
-    public Canvas press;
-    public Rigidbody2D ZaWarudo;
-    
-	// Use this for initialization
-	void Start () {
-        press = press.GetComponent<Canvas>();
-        ZaWarudo = ZaWarudo.GetComponent<Rigidbody2D>();
-        press.enabled = false;
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D col)
+namespace Viaje
+{
+    public class Interact : MonoBehaviour
     {
-        if (col.tag == "Player")
+
+        //variable
+        private bool moveOb = false;
+
+        //reference
+        public Canvas press;
+        public Animator ZaWarudo;
+
+        // Use this for initialization
+        void Start()
         {
-            press.enabled = true;
-        }
-
-        doar = true;
-
-    }
-
-    private void OnTriggerStay2D(Collider2D col)
-    {
-        if (col.tag == "Player")
-        {
-            press.enabled = true;
-        }
-
-        doar = true;
-
-    }
-
-    private void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.tag == "Player")
-        {
+            press = press.GetComponent<Canvas>();
+            ZaWarudo = ZaWarudo.GetComponent<Animator>();
             press.enabled = false;
+
         }
 
-        doar = false;
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.tag == "Player")
+            {
+                press.enabled = true;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    ZaWarudo.SetBool("Open", true);
+                }
+            }
+            
+
+        }
+
+        private void OnTriggerStay2D(Collider2D col)
+        {
+            if (col.tag == "Player")
+            {
+                press.enabled = true;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    ZaWarudo.SetBool("Open", true);
+                }
+                
+            }
+            
+
+        }
+
+        private void OnTriggerExit2D(Collider2D col)
+        {
+            if (col.tag == "Player")
+            {
+                press.enabled = false;
+                ZaWarudo.SetBool("Open", false);
+            }
+            
+        }
+
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
-
-
-
-
-    // Update is called once per frame
-    void Update () {
-        
-	}
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,110 +1,115 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Viaje;
 
-public class PlayerMove : MonoBehaviour {
-
-
-    // variables
-    public Vector2 speed = new Vector2(50,50);
-    public bool puedeSaltar = false;
-    public int salto;
-    public bool giro = false;
-
-    // reference
-    public Rigidbody2D rb;
-    public Animator ani;
-    private Transform july;
-    private Transform direction;
+namespace Viaje {
+    public class PlayerMove : MonoBehaviour
+    {
 
 
+        // variables
+        public Vector2 speed = new Vector2(50, 50);
+        public bool puedeSaltar = false;
+        public int salto;
+        public bool giro = false;
 
-    // Use this for initialization
-    void Start () {
-        
-        ani = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
-        july = this.transform.Find("JulioFull");
+        // reference
+        public Rigidbody2D rb;
+        public Animator ani;
+        private Transform july;
+        private Transform direction;
+
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+            ani = GetComponent<Animator>();
+            rb = GetComponent<Rigidbody2D>();
+            july = this.transform.Find("JulioFull");
+        }
+
+
+        // Ground check
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.tag == "floor")
+            {
+                puedeSaltar = true;
+                ani.SetBool("Saltar", false);
+            }
+        }
+
+        private void OnTriggerStay2D(Collider2D col)
+        {
+            if (col.tag == "floor")
+            {
+                puedeSaltar = true;
+                ani.SetBool("Saltar", false);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D col)
+        {
+            if (col.tag == "floor")
+            {
+                puedeSaltar = false;
+                ani.SetBool("Saltar", true);
+            }
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            //-----Movement-----//
+            float inputx = Input.GetAxisRaw("Horizontal");
+            Vector3 movement = new Vector3(speed.x * inputx, 0, 0);
+            movement *= Time.deltaTime;
+            transform.Translate(movement);
+
+            if (Input.GetKeyDown("right"))
+            {
+                ani.SetBool("Move", true);
+                if (giro)
+                {
+                    july.Rotate(0, 180, 0);
+                    giro = false;
+                }
+            }
+            if (Input.GetKeyDown("left"))
+            {
+                ani.SetBool("Move", true);
+                if (!giro)
+                {
+                    july.Rotate(0, 180, 0);
+                    giro = true;
+                }
+            }
+
+            if (Input.GetKeyUp("left") || Input.GetKeyUp("right"))
+            {
+                ani.SetBool("Move", false);
+            }
+
+
+            //-----Jump-----//
+            if (puedeSaltar)
+            {
+                if (Input.GetKeyDown("space"))
+                {
+                    rb.AddForce(Vector2.up * salto);
+                    
+                }
+            }
+
+        }
+
+
     }
     
-
-    // Ground check
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.tag == "floor")
-        {
-            puedeSaltar = true;
-            ani.SetBool("Saltar", false);
-        }
-    }
-
-    private void OnTriggerStay2D(Collider2D col)
-    {
-        if (col.tag == "floor")
-        {
-            puedeSaltar = true;
-            ani.SetBool("Saltar", false);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.tag == "floor")
-        {
-            puedeSaltar = false;
-            ani.SetBool("Saltar", true);
-        }
-    }
-    
-
-    // Update is called once per frame
-    void Update () {
-        //-----Movement-----//
-        float inputx = Input.GetAxisRaw("Horizontal");
-        Vector3 movement = new Vector3(speed.x * inputx, 0, 0);
-        movement *= Time.deltaTime;
-        transform.Translate(movement);
-
-        if (Input.GetKeyDown("right"))
-        {
-            ani.SetBool("Move", true);
-            if (giro)
-            {
-                july.Rotate(0,180,0);
-                giro = false;
-            }
-        }
-        if (Input.GetKeyDown("left"))
-        {
-            ani.SetBool("Move", true);
-            if (!giro)
-            {
-                july.Rotate(0, 180, 0);
-                giro = true;
-            }
-        }
-
-        if (Input.GetKeyUp("left") || Input.GetKeyUp("right"))
-        {
-            ani.SetBool("Move", false);
-        }
-
-        
-        //-----Jump-----//
-        if (puedeSaltar)
-        {
-            if(Input.GetKeyDown("space"))
-            {
-                rb.AddForce(Vector2.up * salto);
-                
-            }
-        }
-            
-    }
-
-
-
-
 
 
 }
