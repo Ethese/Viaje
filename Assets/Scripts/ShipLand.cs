bcb983eaed6fc8306c94bf51dcbe137bbe61f4be
Cheche <Cheche>
2017-08-21 15:50:24 -0300

    Arreglar Texto

---
+++ Assets/Scripts/ShipLand.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Cheche
+{
+    public class ShipLand : MonoBehaviour
+    {
+        public Animator anim;
+
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+            anim.enabled = true;
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            if (transform.position.y < 32)
+            {
+                anim.enabled = false;
+            }
+
+        }
+
+    }
+}
